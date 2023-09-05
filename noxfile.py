25e022aef97ce78b873f40bf841a9433a2e2f632
Tim Savage <tim@savage.company>
2023-09-06 01:01:40 +1000

    Add noxfile to perform multi-python local tests

---
+++ noxfile.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import nox
+from nox.sessions import Session
+
+HERE = Path(__file__).parent
+
+
+@nox.session(python=("3.8", "3.9", "3.10", "3.11"))
+def tests(session: Session):
+    # fmt: off
+    session.run(
+        "poetry", "export",
+        # "--with=dev",
+        "--output=requirements.txt",
+        external=True,
+    )
+    # fmt: on
+    session.install("-Ur", "requirements.txt")
+    session.run(HERE / "runtests.py")
