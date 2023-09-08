c4170020b2a88331e2652016a08ec9e42de09c73
Joey Jurjens <joey@highbiza.nl>
2023-09-08 10:36:30 +0200

    Setup basic test suite data for tests.

--- tests/resources/test_category.py
+++ tests/resources/test_category.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
 
+from oscar.core.loading import get_model
+
 from oscar_odin import resources
+
+Product = get_model("catalogue", "Product")
 
 
 class TestProduct(TestCase):
-    def setUp(self):
-        pass
+    fixtures = ["oscar_odin/catalogue"]
 
     def test_init(self):
-        resources.category.Product()
+        model_product = Product.objects.first()
+        self.assertNotEqual(model_product, None)
+
+        resource_product = resources.category.Product()
+        # ToDo: Make it so this assert passes
+        self.assertEqual(model_product.title, resource_product.title)
