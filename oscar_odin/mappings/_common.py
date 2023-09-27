d7c48077acff50f62a629d457905e6cda83e7b28
Tim Savage <tim@savage.company>
2023-09-27 23:26:36 +1000

    Default mapping result to immediate results.
    This prevents issues with database sessions going out of scope and causing problems with the internal mapping cache.

--- oscar_odin/mappings/_common.py
+++ oscar_odin/mappings/_common.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, Optional, Type
 
 import odin
 from django.db.models import QuerySet
+from odin.mapping import ImmediateResult
 
 
 def map_queryset(
@@ -13,6 +14,9 @@ def map_queryset(
 ) -> list:
     """Map a queryset to a list of resources.
 
+    This method will call ``QuerySet.all()`` to ensure that the queryset is can
+    be directly iterated.
+
     :param mapping: The mapping type to use.
     :param queryset: The queryset to map.
     :param context: Optional context dictionary to pass to the mapping.
@@ -22,4 +26,6 @@ def map_queryset(
         raise ValueError(
             f"Mapping {mapping} cannot map queryset of type {queryset.model}"
         )
-    return list(mapping.apply(queryset.all(), context=context))
+    return list(
+        mapping.apply(queryset.all(), context=context, mapping_result=ImmediateResult)
+    )
