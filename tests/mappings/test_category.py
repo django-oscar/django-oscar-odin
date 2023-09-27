a1041c665135315d288fcb96c861f861b9ed7cdf
Tim Savage <tim@savage.company>
2023-09-27 23:27:03 +1000

    Some high level tests of mapping of products

--- tests/mappings/test_category.py
+++ tests/mappings/test_category.py
@@ -10,8 +10,32 @@ class TestProduct(TestCase):
     fixtures = ["oscar_odin/catalogue"]
 
     def test_mapping__basic_model_to_resource(self):
-        model_product = Product.objects.first()
+        product = Product.objects.first()
 
-        actual = category.product_to_resource(model_product)
+        actual = category.product_to_resource(product)
 
-        self.assertEqual(model_product.title, actual.title)
+        self.assertEqual(product.title, actual.title)
+
+    def test_mapping__basic_product_with_out_of_stock_children(self):
+        product = Product.objects.get(id=1)
+
+        actual = category.product_to_resource(product)
+
+        self.assertEqual(product.title, actual.title)
+
+    def test_mapping__where_is_a_parent_product_do_not_include_children(self):
+        product = Product.objects.get(id=8)
+
+        actual = category.product_to_resource(product)
+
+        self.assertEqual(product.title, actual.title)
+        self.assertIsNone(actual.children)
+
+    def test_mapping__where_is_a_parent_product_include_children(self):
+        product = Product.objects.get(id=8)
+
+        actual = category.product_to_resource(product, include_children=True)
+
+        self.assertEqual(product.title, actual.title)
+        self.assertIsNotNone(actual.children)
+        self.assertEqual(3, len(actual.children))
