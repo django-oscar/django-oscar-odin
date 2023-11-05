8382fe6914c189279791de054c49ccdb94fa71ea
Tim Savage <tim@savage.company>
2023-11-06 02:29:57 +1100

    Expand resolver to include foreign keys

--- oscar_odin/django_resolver.py
+++ oscar_odin/django_resolver.py
@@ -12,4 +12,10 @@ class ModelFieldResolver(FieldResolverBase):
     def get_field_dict(self) -> Dict[str, Optional[Field]]:
         """Get a dictionary of fields from the source object."""
         meta = getmeta(self.obj)
-        return {f.attname: f for f in meta.fields}
+        fields = {f.attname: f for f in meta.fields}
+        fields.update(
+            (r.related_name, r.field)
+            for r in meta.related_objects
+            if r.related_name != "+"
+        )
+        return fields
