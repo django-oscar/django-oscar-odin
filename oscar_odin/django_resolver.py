78ff4b005e036242f8cba889d7db12dc18835871
Tim Savage <tim@savage.company>
2023-11-23 21:52:06 +1100

    Separate out the behaviours of foreign key fields to specify the related name rather than the attname (which will be the FIELDNAME_id).

--- oscar_odin/django_resolver.py
+++ oscar_odin/django_resolver.py
@@ -1,6 +1,8 @@
 """Resolver for resolving attributes on Django models."""
 from typing import Dict, Optional
 
+from django.db.models import ForeignKey
+
 from odin import Field
 from odin.mapping import FieldResolverBase
 from odin.utils import getmeta
@@ -12,7 +14,11 @@ class ModelFieldResolver(FieldResolverBase):
     def get_field_dict(self) -> Dict[str, Optional[Field]]:
         """Get a dictionary of fields from the source object."""
         meta = getmeta(self.obj)
-        fields = {f.attname: f for f in meta.fields}
+        # Add basic fields
+        fields = {f.attname: f for f in meta.fields if not isinstance(f, ForeignKey)}
+        # Specifically add foreign key fields
+        fields.update((f.name, f) for f in meta.fields if isinstance(f, ForeignKey))
+        # Add related object fields
         fields.update(
             (r.related_name, r.field)
             for r in meta.related_objects
