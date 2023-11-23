20fbeb38f449859536cb73ce7763a0e95ca2d21e
Tim Savage <tim@savage.company>
2023-11-23 21:51:54 +1100

    Restructure the model mapper to provide field instances and parent objects.

--- oscar_odin/mappings/_model_mapper.py
+++ oscar_odin/mappings/_model_mapper.py
@@ -1,7 +1,8 @@
 """Extended model mapper for Django models."""
-from typing import Sequence
+from typing import Sequence, cast
 
-from django.db.models.fields.related import ForeignKey
+from django.db.models.fields.related import ForeignKey, ManyToManyField
+from django.db.models.options import Options
 
 from odin.mapping import MappingBase, MappingMeta
 from odin.utils import getmeta
@@ -16,31 +17,24 @@ class ModelMappingMeta(MappingMeta):
         if mapping_type.to_obj is None:
             return mapping_type
 
+        meta = cast(Options, getmeta(mapping_type.to_obj))
+
         # Extract out foreign field types.
-        mapping_type.one_to_one_fields = one_to_one_fields = []
+        mapping_type.one_to_many_fields = one_to_many_fields = []
         mapping_type.many_to_one_fields = many_to_one_fields = []
         mapping_type.many_to_many_fields = many_to_many_fields = []
-        mapping_type.foreign_key_fields = foreign_key_fields = []
+        mapping_type.foreign_key_fields = [
+            field for field in meta.fields if isinstance(field, ForeignKey)
+        ]
 
-        meta = getmeta(mapping_type.to_obj)
-
+        # Break out related objects by their type
         for relation in meta.related_objects:
             if relation.many_to_many:
-                many_to_many_fields.append(
-                    (relation.related_name, relation.field.attname)
-                )
+                many_to_many_fields.append(relation.field)
             elif relation.many_to_one:
-                many_to_one_fields.append(
-                    (relation.related_name, relation.field.attname)
-                )
+                many_to_one_fields.append(relation.field)
             elif relation.one_to_many:
-                many_to_many_fields.append(
-                    (relation.related_name, relation.field.attname)
-                )
-
-        for field in meta.fields:
-            if isinstance(field, ForeignKey):
-                foreign_key_fields.append((field.attname, field.attname))
+                one_to_many_fields.append(relation.field)
 
         return mapping_type
 
@@ -50,33 +44,37 @@ class ModelMapping(MappingBase, metaclass=ModelMappingMeta):
 
     exclude_fields = []
     mappings = []
-    one_to_one_fields: Sequence[str] = []
-    many_to_one_fields: Sequence[str] = []
-    many_to_many_fields: Sequence[str] = []
-    foreign_key_fields: Sequence[str] = []
+
+    # Specific fields
+    one_to_many_fields: Sequence[ManyToManyField] = []
+    many_to_one_fields: Sequence[ManyToManyField] = []
+    many_to_many_fields: Sequence[ManyToManyField] = []
+    foreign_key_fields: Sequence[ForeignKey] = []
 
     def create_object(self, **field_values):
         """Create a new product model."""
 
-        self.context["one_to_one_items"] = one_to_one_items = [
-            (name, table_name, field_values.pop(name))
-            for name, table_name in self.one_to_one_fields
-            if name in field_values
+        parent = super().create_object(**field_values)
+
+        self.context["one_to_many_items"] = [
+            (parent, field, field_values.pop(field.name))
+            for field in self.one_to_many_fields
+            if field.name in field_values
         ]
-        self.context["many_to_one_items"] = many_to_one_items = [
-            (name, table_name, field_values.pop(name))
-            for name, table_name in self.many_to_one_fields
-            if name in field_values
+        self.context["many_to_one_items"] = [
+            (parent, field, field_values.pop(field.name))
+            for field in self.many_to_one_fields
+            if field.name in field_values
         ]
-        self.context["many_to_many_items"] = many_to_many_items = [
-            (name, table_name, field_values.pop(name))
-            for name, table_name in self.many_to_many_fields
-            if name in field_values
+        self.context["many_to_many_items"] = [
+            (parent, field, field_values.pop(field.name))
+            for field in self.many_to_many_fields
+            if field.name in field_values
         ]
-        self.context["foreign_key_items"] = foreign_key_items = [
-            (name, table_name, field_values.pop(name))
-            for name, table_name in self.foreign_key_fields
-            if name in field_values
+        self.context["foreign_key_items"] = [
+            (parent, field, field_values.pop(field.name))
+            for field in self.foreign_key_fields
+            if field.name in field_values
         ]
 
-        return super().create_object(**field_values)
+        return parent
