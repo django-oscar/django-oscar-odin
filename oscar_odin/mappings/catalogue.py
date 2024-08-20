79e965f8f1ccf75ba00c068c60f4806bd00408b2
Joey <joeyjurjens@gmail.com>
2024-08-20 16:58:45 +0200

    Return price,  currency and num_available even though it's not available to be bought (#32)
    Just because it's not available to be bought, does not mean there's no price, currency, num_available etc.

--- oscar_odin/mappings/catalogue.py
+++ oscar_odin/mappings/catalogue.py
@@ -224,7 +224,7 @@ class ProductToResource(OscarBaseMapping):
     @odin.assign_field(
         to_field=("price", "currency", "availability", "is_available_to_buy")
     )
-    def map_stock_price(self) -> Tuple[Decimal, str, int]:
+    def map_stock_price(self) -> Tuple[Decimal, str, int, bool]:
         """Resolve stock price using strategy and decompose into price/currency/availability."""
         stock_strategy: DefaultStrategy = self.context["stock_strategy"]
 
@@ -233,16 +233,12 @@ class ProductToResource(OscarBaseMapping):
         else:
             price, availability, _ = stock_strategy.fetch_for_product(self.source)
 
-        if availability.is_available_to_buy:
-            return (
-                price.excl_tax,
-                price.currency,
-                getattr(availability, "num_available", 0),
-                True,
-            )
-        else:
-            # There is no stock record for this product.
-            return Decimal(0), "", 0, False
+        return (
+            getattr(price, "excl_tax", Decimal(0)),
+            getattr(price, "currency", ""),
+            getattr(availability, "num_available", 0),
+            availability.is_available_to_buy
+        )
 
 
 class ProductToModel(ModelMapping):
